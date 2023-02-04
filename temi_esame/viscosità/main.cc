87e60700d2fe9daaff03cdf4056af94e65e532d5
Eleonora Redaelli <122676437+elered@users.noreply.github.com>
2023-02-04 18:05:39 +0100

    Add files via upload

---
+++ temi_esame/viscosità/main.cc
@@ -0,0 +1,22 @@
+#include "TApplication.h"
+#include "TCanvas.h" 
+#include "TH1F.h"
+#include "TAxis.h"
+#include "TGraph.h"
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <iostream>
+#include "randomgen.h"
+#include "visco.h"
+
+using namespace std;
+
+int main(){
+  
+  int n = 1000;
+
+  app.Run();
+
+  return 0;
+}
